46ac40f26651ea58fae228c149ee9aa7188f024d
lucasgpereira010 <lucasgp010@outlook.com>
2019-04-22 16:17:29 -0300

    Aula de hoje

---
+++ src/pessoa/Execução.java
@@ -0,0 +1,16 @@
+package pessoa;
+
+import pessoa.Pessoa;
+
+public class Execução {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pessoa pes = new Pessoa();
+		System.out.println(pes.andar("x"));
+		System.out.println(pes.respirar("y"));
+		//System.out.println(pes.dormir("z"));
+
+	}
+
+}
---
+++ src/pessoa/Pessoa.java
@@ -0,0 +1,23 @@
+package pessoa;
+
+public class Pessoa {
+	
+	public String andar(String y) {
+		y="andando";
+		return y;
+	}
+	protected String respirar(String y) {
+		y="respirando";
+		return y;
+	}
+	private String dormir(String z) {
+		z="dormindo";
+		return z;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
